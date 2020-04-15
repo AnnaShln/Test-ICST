711505bfc82b25473068050711d59cb90d9f0580
AnnaShln <anyashilnikova0708@gmail.com>
2020-04-15 16:48:56 +0300

    first try

--- app/src/main/java/com/example/testicst/Question1.java
+++ app/src/main/java/com/example/testicst/Question1.java
@@ -10,5 +10,7 @@ public class Question1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_quest1);
+
+
     }
 }
