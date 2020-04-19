0bb92ccd801bbd5429de492a801ee1caf38a881f
AnnaShln <anyashilnikova0708@gmail.com>
2020-04-19 16:59:24 +0300

    добавила комментарии

--- app/src/main/java/com/example/testicst/Question1.java
+++ app/src/main/java/com/example/testicst/Question1.java
@@ -27,48 +27,48 @@ public class Question1 extends AppCompatActivity {
         Button secondAns = findViewById(R.id.quest1_ans2);
         Button thirdAns = findViewById(R.id.quest1_ans3);
 
-        int c = getNumber(counter);
+        int c = getNumber(counter);                  //достаю номер вопроса
 
-        if (c < 20) {
+        if (c < 20) {                                //если номер вопроса меньше 20 формирую его, если нет возвращаюсь на тестовую страничку
 
             String s = getStringResourceByName(createCounter(c + 1));
             String p = getStringResourceByName(createQuestion(c + 1));
             counter.setText(s);
             question.setText(p);
 
-            if (c == 4 || c == 6 || c == 8 || c == 14 || c == 18) {
+            if (c == 4 || c == 6 || c == 8 || c == 14 || c == 18) {                    //это вопросы в которых три варианта ответа
                 String ans1 = getStringResourceByName(createAns1(c + 1));
                 String ans2 = getStringResourceByName(createAns2(c + 1));
                 String ans3 = getStringResourceByName(createAns3(c + 1));
                 firstAns.setText(ans1);
                 secondAns.setText(ans2);
                 thirdAns.setText(ans3);
-                thirdAns.setVisibility(View.VISIBLE);
+                thirdAns.setVisibility(View.VISIBLE);                                  //поэтому третий вариант виден
             } else {
                 String ans1 = getStringResourceByName(createAns1(c + 1));
                 String ans2 = getStringResourceByName(createAns2(c + 1));
                 firstAns.setText(ans1);
                 secondAns.setText(ans2);
-                thirdAns.setVisibility(View.GONE);
+                thirdAns.setVisibility(View.GONE);                                     //а тут не виден
             }
         } else {
             next.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Intent intent = new Intent(Question1.this, MainActivity.class);
-                    startActivity(intent);
+                public void onClick(View v) {                                             //возвращаемся на фрагмент теста,
+                    Intent intent = new Intent(Question1.this, MainActivity.class);    //я еще не прописала его изменение в зависимости от прохождения теста,
+                    startActivity(intent);                                                           //или же создам новый, я пока не решила как лучше
                 }
             });
         }
     }
 
-    private String getStringResourceByName(String aString) {
+    private String getStringResourceByName(String aString) {          //этот метод чтобы получить стринговое содержание по имени
         String packageName = getPackageName();
         int resId = getResources().getIdentifier(aString, "string", packageName);
         return getString(resId);
     }
 
-    private String createCounter (int c) {
-        return "counter" + Integer.toString(c);
+    private String createCounter (int c) {                  //в этих методах я формирую name нужного мне стринга в зависимости от номера вопроса.
+        return "counter" + Integer.toString(c);             // так как они однотипные
     }
 
     private String createQuestion (int c) {
@@ -87,8 +87,8 @@ public class Question1 extends AppCompatActivity {
         return "quest" + Integer.toString(c) + "ans3";
     }
 
-    private int getNumber (TextView textView){
-        CharSequence charSeq = textView.getText();
+    private int getNumber (TextView textView){             //для изменения текста везде (счетчик, вопрос, ответы) мне надо было получить номер
+        CharSequence charSeq = textView.getText();         //вопроса, я беру его из счетчика, и здесь короче читаю ео первые символы как раз для этого
         String str = charSeq.toString();
         int numb1 = Character.getNumericValue(str.charAt(0));
         if (str.charAt(1) != '/') {
